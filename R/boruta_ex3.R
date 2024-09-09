4432351710d971812601cae5e85ae4d83b3d3377
brandmaier <brandmaier@mpib-berlin.mpg.de>
2024-09-09 15:06:59 +0200

    boruta results

--- R/boruta_ex3.R
+++ R/boruta_ex3.R
@@ -1,11 +1,23 @@
+library(semtree)
+
+if (!dir.exists("data")) dir.create("data")
+if (!file.exists("data/DASS_data_21.02.19.zip")) download.file("https://openpsychometrics.org/_rawdata/DASS_data_21.02.19.zip",destfile="data/DASS_data_21.02.19.zip")
 if (!exists("DASS")) DASS<-read.csv("data/DASS_data_21.02.19/data.csv",sep="\t")
 names(DASS)
 
 items <- DASS[,c((1:42)-1)*3+1 ]
 sumscore <- rowSums(items)
 
-predictors <- DASS[,c("education","urban","gender",
-                      "engnat","age","hand","voted","married")]
+predictors <- DASS[,c("education",#"urban",
+                      "gender",
+                      #"engnat",
+                      "age",
+                      "hand",
+                      "familysize",
+                      "screensize"
+                      #"voted",
+                    #  "married")
+                   )]
 
 DASS_sum <- cbind( sumscore, predictors)
 DASS_sum <- DASS_sum[complete.cases(DASS_sum),]
@@ -13,18 +25,19 @@ DASS_sum <- DASS_sum[complete.cases(DASS_sum),]
 DASS_sum$education <- ordered(DASS_sum$education, levels=1:4,
                               labels=c(
                                 "Less than high school", "High school", "University degree", "Graduate degree"))
-DASS_sum$urban <- factor(DASS_sum$urban, levels=c(1,2,3),
-                         labels=c("Rural (country side)", "Suburban", "Urban (town, city)"))
+#DASS_sum$urban <- factor(DASS_sum$urban, levels=c(1,2,3),
+#                         labels=c("Rural (country side)", "Suburban", "Urban (town, city)"))
 DASS_sum$gender <- factor(DASS_sum$gender,levels = c(1,2,3),
                           labels=c("male","female","other"))
-DASS_sum$engnat <- factor(DASS_sum$engnat,levels=c(1,2),
-                          labels=c("yes","no"))
+#DASS_sum$engnat <- factor(DASS_sum$engnat,levels=c(1,2),
+#                          labels=c("yes","no"))
 DASS_sum$hand <- factor(DASS_sum$hand, levels=1:3,
                         labels=c("right","left","both"))
-DASS_sum$voted <- factor(DASS_sum$voted, levels=1:2,labels=c("yes","no"))
-DASS_sum$married <- factor(DASS_sum$married,levels=1:3,
-                           labels=c("never","currently","previously"))
+#DASS_sum$voted <- factor(DASS_sum$voted, levels=1:2,labels=c("yes","no"))
+#DASS_sum$married <- factor(DASS_sum$married,levels=1:3,
+#                           labels=c("never","currently","previously"))
 #DASS_sum$religion <- factor(DASS_sum$religion)
+DASS_sum$screensize <- factor(DASS_sum$screensize)
 
 DASS_sum <- DASS_sum[complete.cases(DASS_sum),]
 manifests<-c("sumscore")
@@ -54,9 +67,10 @@ tree<-semtree(model, DASS_sum,
 
 # forest dauert 20 Minuten pro Stück... etwa 4min/tree... warum?! 
 library(future)
-#plan(multisession, workers = 4)
+plan(multisession, workers = 15)
 boruta_results <- boruta(model, DASS_sum, 
-                         control=semforest_score_control(),
+                         control=semforest_score_control(num.trees=20,
+                           control=semtree_control(max.depth=4)),
                          maxRuns = 11)
 
 if (!dir.exists("results/ex3")) dir.create("results/ex3")
